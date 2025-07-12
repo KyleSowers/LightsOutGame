73c2c6334250b3eee831af39a3eec708c7f72caa
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-07-11 23:36:12 -0400

    Setting up ColorActivity.Getting back a result. Getting the selected color.

--- app/src/main/java/com/zybooks/lightsout/ColorActivity.java
+++ app/src/main/java/com/zybooks/lightsout/ColorActivity.java
@@ -1,6 +1,8 @@
 package com.zybooks.lightsout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ColorActivity extends AppCompatActivity {
 
+    public static final String EXTRA_COLOR = "com.zybooks.lightsout.color";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_color);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    public void onColorSelected(View view) {
+        int colorId = R.color.yellow;
+        if (view.getId() == R.id.radio_red) {
+            colorId = R.color.red;
+        } else if (view.getId() == R.id.radio_orange) {
+            colorId = R.color.orange;
+        } else if (view.getId() == R.id.radio_green) {
+            colorId = R.color.green;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_COLOR, colorId);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
 }
--- app/src/main/java/com/zybooks/lightsout/MainActivity.java
+++ app/src/main/java/com/zybooks/lightsout/MainActivity.java
@@ -1,11 +1,16 @@
 package com.zybooks.lightsout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -111,6 +116,27 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HelpActivity.class);
         startActivity(intent);
     }
+
+    public void onChangeColorClick(View view) {
+        Intent intent = new Intent(this, ColorActivity.class);
+        mColorResultLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> mColorResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            int colorID = data.getIntExtra(ColorActivity.EXTRA_COLOR, R.color.yellow);
+                            mLightOnColor = ContextCompat.getColor(MainActivity.this, colorID);
+                            setButtonColors();
+                        }
+                    }
+                }
+            });
 
 }
 
